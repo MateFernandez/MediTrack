8cecdd1aaa38b9eef258befc449c2b123410a844
Juan Manuel Fernández Bogo <144643059+JuanManuelFernandez@users.noreply.github.com>
2026-03-02 18:51:24 -0300

    Cambios en el front

--- app/src/main/java/com/example/meditrack/MainActivity.java
+++ app/src/main/java/com/example/meditrack/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.meditrack;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ListView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -27,10 +30,61 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textRecordatorio = findViewById(R.id.textRecordatorio);
 
-        // PRUEBA
-        String medicamento = "Ibuprofeno";
-        String horaProgramada = "02:14";
+        mostrarRecordatorio(textRecordatorio, "Ibuprofeno", "20:21");
 
+        CambiarTitulo();
+
+        ArrayList<String> listaMedicamentos = new ArrayList<>();
+        listaMedicamentos.add("Ibuprofeno - 20:21");
+        listaMedicamentos.add("Paracetamol - 08:00");
+        listaMedicamentos.add("Amoxicilina - 12:30");
+        listaMedicamentos.add("Omeprazol - 07:00");
+        listaMedicamentos.add("Aspirina - 09:15");
+        listaMedicamentos.add("Metformina - 18:00");
+        listaMedicamentos.add("Losartán - 22:00");
+        listaMedicamentos.add("Atorvastatina - 21:00");
+        listaMedicamentos.add("Clonazepam - 23:00");
+        listaMedicamentos.add("Vitamina D - 10:00");
+        listaMedicamentos.add("Hierro - 14:00");
+        listaMedicamentos.add("Ibuprofeno - 20:21");
+        listaMedicamentos.add("Paracetamol - 08:00");
+        listaMedicamentos.add("Amoxicilina - 12:30");
+        listaMedicamentos.add("Omeprazol - 07:00");
+        listaMedicamentos.add("Aspirina - 09:15");
+        listaMedicamentos.add("Metformina - 18:00");
+        listaMedicamentos.add("Losartán - 22:00");
+        listaMedicamentos.add("Atorvastatina - 21:00");
+        listaMedicamentos.add("Clonazepam - 23:00");
+        listaMedicamentos.add("Vitamina D - 10:00");
+        listaMedicamentos.add("Hierro - 14:00");
+
+        // Configurar ListView
+        ListView listView = findViewById(R.id.listaMedicamentos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.medicamento,
+                R.id.textMedicamento,
+                listaMedicamentos
+        );
+        listView.setAdapter(adapter);
+
+    }
+    private void CambiarTitulo() {
+        TextView titulo = findViewById(R.id.textView3); //OBTENER REFERENCIA
+
+        String horaActual = new SimpleDateFormat("HH", Locale.getDefault()).format(new Date());
+        int hora = Integer.parseInt(horaActual);
+
+        // Cambiar el texto según la hora
+        if (hora >= 6 && hora < 12) {
+            titulo.setText("Buenos días");
+        } else if (hora >= 12 && hora < 20) {
+            titulo.setText("Buenas tardes");
+        } else {
+            titulo.setText("Buenas noches");
+        }
+    }
+    private void mostrarRecordatorio(TextView textRecordatorio, String medicamento, String horaProgramada) {
         String horaActual = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
 
         if (horaActual.equals(horaProgramada)) {
@@ -38,6 +92,5 @@ public class MainActivity extends AppCompatActivity {
         } else {
             textRecordatorio.setText("No hay recordatorios en este momento");
         }
-
     }
 }
